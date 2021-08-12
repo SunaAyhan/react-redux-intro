b224ee498fdbae70b69843d20ba4726f3c1fa4e2
SunaAyhan <sunaayhan16@gmail.com>
2021-08-12 14:25:22 +0300

    redux last commit

---
+++ src/components/IncreaseByTwoCounter.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+
+import { connect } from 'react-redux'
+import {increaseByTwoCounter} from '../redux/actions/counterActions'
+import { bindActionCreators } from 'redux'
+class IncreaseByTwoCounter extends Component {
+    render() {
+        return (
+            <div>
+                 <button onClick={e=>{
+                    this.props.dispatch(increaseByTwoCounter());
+                }}>
+                    2 arttır
+                </button>  
+            </div>
+        )
+    }
+}
+function mapDispatchToProps(dispatch){
+    return {actions:bindActionCreators(increaseByTwoCounter, dispatch)}
+    }
+    
+    export default connect(mapDispatchToProps)(IncreaseByTwoCounter);
---
+++ src/components/IncreaseCounter.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import {increaseCounter} from '../redux/actions/counterActions'
+import { bindActionCreators } from 'redux'
+class IncreaseCounter extends Component {
+    render() {
+        return (
+            <div>
+                <button onClick={e=>{
+                    this.props.dispatch(increaseCounter());
+                }}>
+                    1 arttır
+                </button>
+            </div>
+        )
+    }
+}
+
+function mapDispatchToProps(dispatch){
+return {actions:bindActionCreators(increaseCounter, dispatch)}
+}
+
+export default connect(mapDispatchToProps)(IncreaseCounter);
